0d73e4e43324e72c2120b3c333e390bbccc32e81
EduCastroo <educastro._@hotmail.com>
2021-11-04 10:15:13 -0300

    add TASK 2

---
+++ ProjetoIntegradorGEN/TASK 2/mangueTech/src/main/java/com/manguetech/MangueTechApplication.java
@@ -0,0 +1,14 @@
+package com.manguetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangueTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangueTechApplication.class, args);
+		
+	}
+
+}
---
+++ ProjetoIntegradorGEN/TASK 2/mangueTech/src/test/java/com/manguetech/mangueTech/MangueTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manguetech.mangueTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangueTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
